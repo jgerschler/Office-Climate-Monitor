62f2e8e1938f0e1295af28ae11d10750d669e5e2
jgerschler <jjgerschler@gmail.com>
2017-02-01 10:17:22 -0600

    updated

---
+++ LCDTesting/LCDTesting.ino
@@ -0,0 +1,21 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C LCD(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
+
+void setup() {
+    LCD.begin(16, 2);  // 16 lines by 2 rows
+    LCD.clear();
+    LCD.backlight();
+    LCD.setCursor(0,0);
+    LCD.print("Hello Line 1");
+    LCD.setCursor(0,1);
+    LCD.print("Hello Line 2");
+}
+
+void loop() {
+    delay(500);
+    LCD.clear();
+    LCD.home();
+    LCD.print(millis());
+}
