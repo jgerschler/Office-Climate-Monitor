d4739f16aafc34705d459b9141f35113466aa437
jgerschler <jjgerschler@gmail.com>
2017-02-01 18:54:43 -0600

    updated

--- EnvironmentalMonitor/EnvironmentalMonitor.ino
+++ EnvironmentalMonitor/EnvironmentalMonitor.ino
@@ -1,22 +1,27 @@
 /*Things that need to be done:
 Set up millis timers:
   1)Check every ten seconds if lights have been turned off.
+    a)inside loop, check if analog voltage has changed past threshold.
+      i)if yes, then turn on LCD backlight, and set pins high for ambient lighting.
+      ii)if lights are already on, keep on, etc.
   1.5)Switch every ten seconds to secondary OLED display.
+    a)plot data held in respective array.
   2)Check every minute for new environmental data.
+    a)update array
+    b)update LCD
   3)Plot every minute environmental data.
-  
-
-
 */
 
-#include <SPI.h>
+//#include <SPI.h>//do we need this?
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <LiquidCrystal_I2C.h>
 
 #define OLED_RESET 4
 
 Adafruit_SSD1306 display(OLED_RESET);
+LiquidCrystal_I2C LCD(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 int temperature_array[121];
 int pressure_array[121];
@@ -27,10 +32,21 @@ int max_val;
 int value = 1;
 
 void setup()   {                
-  Serial.begin(9600);
-
+//  Serial.begin(9600);
+  //get data here before updating displays
+  LCD.begin(20, 4);
+  LCD.clear();
+  LCD.backlight();
+  LCD.setCursor(0,0);
+  LCD.print("Temp:");//add variables here
+  LCD.setCursor(0,1);
+  LCD.print("Humidity:");
+  LCD.setCursor(0,2);
+  LCD.print("Pressure:");
+  LCD.setCursor(0,3);
+  LCD.print("Altitude:");
+  
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-
   display.clearDisplay();
 
   data_plot();
@@ -40,11 +56,24 @@ void setup()   {
 
 }
 
+void update_lcd {
+  LCD.clear();
+  LCD.setCursor(0,0);
+  LCD.print("Temp:");//add variables here
+  LCD.setCursor(0,1);
+  LCD.print("Humidity:");
+  LCD.setCursor(0,2);
+  LCD.print("Pressure:");
+  LCD.setCursor(0,3);
+  LCD.print("Altitude:");
+}
+
+
 void loop() {
   
 }
 
-void data_plot() {
+void update_oled() {
   if (min_y == max_y) {
     min_y = floor(value*0.5);
     max_y = ceil(value*1.5);
