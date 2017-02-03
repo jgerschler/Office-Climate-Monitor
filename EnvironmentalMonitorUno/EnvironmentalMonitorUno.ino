5e98c594df9957ae0ecc33b377b0d4e1c0718251
jgerschler <jjgerschler@gmail.com>
2017-02-02 18:16:36 -0600

    rough but working

--- EnvironmentalMonitorUno/EnvironmentalMonitorUno.ino
+++ EnvironmentalMonitorUno/EnvironmentalMonitorUno.ino
@@ -1,3 +1,6 @@
+/*A smaller version for the Arduino UNO that doesn't
+  show the pressure plot data.*/
+
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -13,27 +16,22 @@ Adafruit_SSD1306 display(OLED_RESET);
 LiquidCrystal_I2C LCD(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 int temp_array[121];
-int pres_array[121];
 int hum_array[121];
 int min_temp_y = 0;
 int max_temp_y = 0;
 int min_hum_y = 0;
 int max_hum_y = 0;
-int min_pres_y = 0;
-int max_pres_y = 0;
 int min_temp_val;
 int max_temp_val;
 int min_hum_val;
 int max_hum_val;
-int min_pres_val;
-int max_pres_val;
 float val_temp;
 float val_hum;
 float val_pres;
 float val_alt;
 unsigned long previousMillis = 0;
 byte index = 0;
-byte oled_disp = 0;
+bool oled_disp = 0;
 
 void setup()   {
   bme.begin();
@@ -83,10 +81,6 @@ void loop() {
       temp_oled();
       oled_disp = 1;
     }
-    else if (oled_disp == 1) {
-      pres_oled();
-      oled_disp = 2;
-    }
     else {
       hum_oled();
       oled_disp = 0;
@@ -99,18 +93,15 @@ void loop() {
     //check if there is space in array
     if (index <= 120) {
       temp_array[index] = round(val_temp);
-      pres_array[index] = round(val_pres);
       hum_array[index] = round(val_hum);
       index+=1;
     }
     else {
       for (int i=0; i<120; i++) {
         temp_array[i] = temp_array[i+1];
-        pres_array[i] = pres_array[i+1];
         hum_array[i] = hum_array[i+1];
       }
       temp_array[120] = round(val_temp);
-      pres_array[120] = round(val_pres);
       hum_array[120] = round(val_hum);
     }
   }
@@ -164,57 +155,6 @@ void temp_oled() {
     display.println(String(min_temp_y));
     for (int i=0; i<=120; i++) {
       display.drawPixel(i+1, 41*(max_temp_y - temp_array[i])/(max_temp_y - min_temp_y) + 19, WHITE);
-    }
-    display.display();
-  }
-}
-
-void pres_oled() {
-  if (min_pres_y == max_pres_y) {
-    min_pres_y = floor(val_pres * 0.5);
-    max_pres_y = ceil(val_pres * 1.5);
-    min_pres_val = val_pres;
-    max_pres_val = val_pres;
-  }
-  if (val_pres > max_pres_val) {
-    max_pres_val = val_pres;
-    max_pres_y = ceil(val_pres * 1.5);
-  }
-  if (val_pres < min_pres_val) {
-    min_pres_val = val_pres;
-    min_pres_y = floor(val_pres * 0.5);
-  }
-  if (index-1 <= 120) {
-    display.setTextSize(2);
-    display.setTextColor(WHITE);
-    display.setCursor(0, 0);
-    display.clearDisplay();
-    display.println("Pressure:");
-    display.drawLine(0, 19, 0, 60, WHITE);
-    display.setTextSize(1);
-    display.setCursor(3, 16);
-    display.println(String(max_pres_y));
-    display.setCursor(3, 57);
-    display.println(String(min_pres_y));
-    for (int i=0; i<=index-1; i++) {
-      display.drawPixel(i+1, 41*(max_pres_y - pres_array[i])/(max_pres_y - min_pres_y) + 19, WHITE);
-    }
-    display.display();
-  }
-  else {
-    display.setTextSize(2);
-    display.setTextColor(WHITE);
-    display.setCursor(0, 0);
-    display.clearDisplay();
-    display.println("Pressure:");
-    display.drawLine(0, 19, 0, 60, WHITE);
-    display.setTextSize(1);
-    display.setCursor(3, 16);
-    display.println(String(max_pres_y));
-    display.setCursor(3, 57);
-    display.println(String(min_pres_y));
-    for (int i=0; i<=120; i++) {
-      display.drawPixel(i+1, 41*(max_pres_y - pres_array[i])/(max_pres_y - min_pres_y) + 19, WHITE);
     }
     display.display();
   }
